b387ab2b98319a8813177498a32881d2582e3356
Koji E. Kosugi <kosugitti@gmail.com>
2024-11-16 10:35:49 +0900

    site update

--- tools/build_site.R
+++ tools/build_site.R
@@ -10,38 +10,56 @@ build_site <- function() {
   message("Building docs site...")
   setwd("docs")
 
+  # 画像ファイルのディレクトリを作成
+  dir.create("figures", showWarnings = FALSE)
+
   # 英語版
   rmarkdown::render("index.Rmd",
                     output_format = "html_document",
+                    output_options = list(
+                      self_contained = FALSE,
+                      lib_dir = "libs",
+                      fig_path = "figures/"
+                    ),
                     output_file = "index.html")
 
   # 日本語版
   rmarkdown::render("ja.Rmd",
                     output_format = "html_document",
+                    output_options = list(
+                      self_contained = FALSE,
+                      lib_dir = "libs",
+                      fig_path = "figures/"
+                    ),
                     output_file = "ja.html")
 
   # ルートディレクトリに戻る
   setwd(old_dir)
 
-  # index.RmdをREADME.Rmdとしてコピー
+  # 一時的にREADME.Rmdを作成
   message("Creating README.md...")
-  file.copy("docs/index.Rmd", "README.Rmd", overwrite = TRUE)
+  temp_rmd <- tempfile(fileext = ".Rmd")
+  on.exit(unlink(temp_rmd), add = TRUE)
 
-  # README.RmdのYAMLヘッダーを修正
-  content <- readLines("README.Rmd")
+  file.copy("docs/index.Rmd", temp_rmd)
+
+  # 一時ファイルのYAMLヘッダーを修正
+  content <- readLines(temp_rmd)
   yaml_end <- which(content == "---")[2]
   new_content <- c(
     "---",
-    "output: github_document",
+    "output:",
+    "  github_document",
     "---",
     content[(yaml_end + 1):length(content)]
   )
-  writeLines(new_content, "README.Rmd")
+  writeLines(new_content, temp_rmd)
 
   # README.mdを生成
-  rmarkdown::render("README.Rmd",
+  rmarkdown::render(temp_rmd,
                     output_format = "github_document",
                     output_file = "README.md")
+
 
   message("Documentation built successfully!")
 }

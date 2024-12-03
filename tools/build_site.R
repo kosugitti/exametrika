6bd25bda7af79c02e36c8aff82a366102be82087
kosugitti <kosugitti@gmail.com>
2024-12-03 15:00:08 +0900

    casch system

--- tools/build_site.R
+++ tools/build_site.R
@@ -8,27 +8,6 @@ build_site <- function() {
   # docsディレクトリに移動
   setwd("docs")
 
-  # setup chunkの内容を確認
-  message("\nChecking Rmd files setup chunks:")
-
-  # index.Rmdのsetup確認
-  message("\nindex.Rmd setup:")
-  index_content <- readLines("index.Rmd")
-  setup_start <- which(grepl("^```\\{r setup,", index_content))
-  if (length(setup_start) > 0) {
-    setup_end <- setup_start + which(grepl("^```$", index_content[setup_start:length(index_content)]))[1] - 1
-    writeLines(index_content[setup_start:setup_end])
-  }
-
-  # ja.Rmdのsetup確認
-  message("\nja.Rmd setup:")
-  ja_content <- readLines("ja.Rmd")
-  setup_start <- which(grepl("^```\\{r setup,", ja_content))
-  if (length(setup_start) > 0) {
-    setup_end <- setup_start + which(grepl("^```$", ja_content[setup_start:length(ja_content)]))[1] - 1
-    writeLines(ja_content[setup_start:setup_end])
-  }
-
   # HTML生成
   message("\nGenerating HTML files...")
 
@@ -42,15 +21,6 @@ build_site <- function() {
                     output_format = "html_document",
                     output_file = "ja.html")
 
-  # 生成された図を確認
-  message("\nChecking generated figures:")
-  if (dir.exists("figures")) {
-    fig_files <- list.files("figures", full.names = TRUE)
-    message("Found figures: ", paste(basename(fig_files), collapse = ", "))
-  } else {
-    message("No figures directory found")
-  }
-
   # READMEの生成
   setwd(old_dir)
   message("\nCreating README.md...")
@@ -58,12 +28,21 @@ build_site <- function() {
   content <- readLines("docs/index.Rmd")
   yaml_end <- which(content == "---")[2]
   content <- content[(yaml_end + 1):length(content)]
-  # setup chunkを削除
-  setup_start <- which(grepl("^```\\{r setup,", content))
+
+  # setup chunkを削除（新しいパターンに対応）
+  setup_start <- which(grepl("^```\\{r setup,|^```\\{r setup\\}", content))
   if (length(setup_start) > 0) {
     setup_end <- setup_start + which(grepl("^```$", content[setup_start:length(content)]))[1] - 1
     content <- content[-(setup_start:setup_end)]
   }
+
+  # title-with-logoのdivとその中身を削除
+  title_div_start <- which(grepl("<div class=\"title-with-logo\">", content))
+  if (length(title_div_start) > 0) {
+    title_div_end <- title_div_start + which(grepl("</div>", content[title_div_start:length(content)]))[1]
+    content <- content[-(title_div_start:title_div_end)]
+  }
+
   # 言語切り替えリンクと前の空行を削除
   lang_link_idx <- grep("\\[English\\].*\\[日本語\\]", content)
   if (length(lang_link_idx) > 0) {
@@ -78,6 +57,7 @@ build_site <- function() {
     lines_to_remove <- c(empty_lines, lang_link_idx)
     content <- content[-lines_to_remove]
   }
+
   writeLines(content, "README.md")
   message("\nDocumentation built successfully!")
 }

2efeaffae8bb89665804a4dc3b705171c67c6b25
kosugitti <kosugitti@gmail.com>
2025-08-13 10:55:13 +0900

    Rd変更

--- tools/build_pkg.R
+++ tools/build_pkg.R
@@ -1,7 +1,7 @@
 styler::style_pkg()
 devtools::check()
 devtools::spell_check()
-rhub::rhub_doctor()
+# rhub::rhub_doctor()
 rhub::rhub_check(platforms = c("linux", "macos", "macos-arm64", "windows"))
 devtools::check_win_devel(email = "kosugitti@gmail.com")
 

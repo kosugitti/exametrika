9bd44bef05a173bb9bcc3fc8c3aa1b732889abb0
kosugitti <kosugitti@gmail.com>
2025-08-18 15:33:30 +0900

    tools

--- tools/build_pkg.R
+++ tools/build_pkg.R
@@ -6,4 +6,4 @@ devtools::spell_check()
 rhub::rhub_check(platforms = c("linux", "macos", "macos-arm64", "windows"))
 devtools::check_win_devel(email = "kosugitti@gmail.com")
 
-#devtools::release()
+devtools::release()
